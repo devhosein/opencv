99b3262a13c3acd8a9c11ae52dc70f4a3c2648fd
Your Name <m22h22b22@gmail.com>
2024-02-11 14:29:19 +0330

    pil

--- p3/pilow.py
+++ p3/pilow.py
@@ -1,9 +1,9 @@
 from PIL import Image
 
 image1 = Image.open("A:\Python\MY_projects\opencv\p3\image1.jpg")
-# تبدیل فرمت
-image1.save("A:\Python\MY_projects\opencv\p3\image1.png")
-# نمایش عکس 
-image1.show(image1)
-
-
+# # تبدیل فرمت
+# image1.save("A:\Python\MY_projects\opencv\p3\image1.png")
+# # نمایش عکس 
+# image1.show(image1)
+# # image rotate
+# image1.rotate(90).save('image_rotated.png')

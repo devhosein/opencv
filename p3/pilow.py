6ca8b7186e66bec43da7652c61e1537e22795c24
Your Name <m22h22b22@gmail.com>
2024-02-11 14:27:05 +0330

    pil

--- p3/pilow.py
+++ p3/pilow.py
@@ -1,6 +1,9 @@
 from PIL import Image
 
 image1 = Image.open("A:\Python\MY_projects\opencv\p3\image1.jpg")
+# تبدیل فرمت
+image1.save("A:\Python\MY_projects\opencv\p3\image1.png")
+# نمایش عکس 
 image1.show(image1)
 
 

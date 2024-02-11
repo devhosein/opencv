84c5f43cb9e5976be8b0a7748505c5fb9aac427d
Your Name <m22h22b22@gmail.com>
2024-02-11 15:01:50 +0330

    work with cv2

---
+++ p4/work_cv.py
@@ -0,0 +1,15 @@
+# https://www.youtube.com/watch?v=kSqxn6zGE0c
+import pandas as pd
+import numpy as np
+from glob import glob
+import cv2
+import matplotlib.pylab as plt
+
+# read the image
+image1 = cv2.imread("A:\Python\MY_projects\opencv\p3\image1.jpg")
+cv2.imshow("iamge", image1)
+cv2.waitke(5000)
+
+
+
+

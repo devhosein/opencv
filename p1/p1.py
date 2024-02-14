859d5e3c0cacce200f38f9b39f1b60275463b708
Your Name <m22h22b22@gmail.com>
2024-02-14 19:34:08 +0330

    ready 1

--- p1/p1.py
+++ p1/p1.py
@@ -1,5 +1,7 @@
 import cv2
-
+import numpy
+import face_recognition
+import time
 
 
 #face_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
@@ -29,7 +31,8 @@ while cap.isOpened:
         cv2.rectangle(img , (x,y), (x+w , y+h), (0,0,255),3)
     
 
-    cv2.imshow("img", img)
+    # کد نشان دادن تصویر
+#    cv2.imshow("img", img)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
     
@@ -44,12 +47,34 @@ while cap.isOpened:
         if result:
             cv2.imwrite("A:\Python\MY_projects\opencv\p1\hello.png", img)
             
-        
+            
     
     
+    # مقایسه دو چهره
 
+    # بارگذاری تصاویر دو چهره
+    image1 = face_recognition.load_image_file("a:/Python/MY_projects/opencv/p1/image (1).jpg")
+    image2 = face_recognition.load_image_file("a:/Python/MY_projects/opencv/p1/hello.png")
+
+    # استخراج ویژگی‌های چهره‌ها
+    face_encoding1 = face_recognition.face_encodings(image1)[0]
+    face_encoding2 = face_recognition.face_encodings(image2)[0]
+
+    # مقایسه دو چهره با یک آستانه 0.6
+    results = face_recognition.compare_faces([face_encoding1], face_encoding2, tolerance=0.6)
+
+    # چاپ نتیجه
+
+    if results[0]:
+        print("same")
+    else:
+        print("difrent")
+        
+    time.sleep(0.5)
+
+                
+            
 
 cap.release()
-
 
 
